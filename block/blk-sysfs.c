6d8456cf602c022ce22d2aa30a90618cbf75b270
Park Ju Hyung <qkrwngud825@gmail.com>
2019-09-18 01:48:43 +0900

    block: hack read_ahead_kb to be always 128 kB
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    Signed-off-by: shygosh <shygosh@proton.me>

--- block/blk-sysfs.c
+++ block/blk-sysfs.c
@@ -110,6 +110,7 @@ queue_ra_store(struct request_queue *q, const char *page, size_t count)
 	if (task_is_booster(current))
 		ra_kb = VM_MAX_READAHEAD;
 
+	ra_kb = 128;
 	q->backing_dev_info->ra_pages = ra_kb >> (PAGE_SHIFT - 10);
 
 	return ret;
