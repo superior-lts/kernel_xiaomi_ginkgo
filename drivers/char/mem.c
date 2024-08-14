41d7e20d62d89fc8c9ef758bfa0b8a35eb6d7a49
dhelo11 <dhelo11@No-Mercy>
2024-08-14 14:17:09 +0000

    Revert "char: mem: register /dev/hw_random through devmem framework when HW_RANDOM is unset"
    This reverts commit 0ce239a67952cb155db52904849a857b9d644f1e.

--- drivers/char/mem.c
+++ drivers/char/mem.c
@@ -892,8 +892,13 @@ static const struct memdev {
 	 [5] = { "zero", 0666, &zero_fops, 0 },
 	 [7] = { "full", 0666, &full_fops, 0 },
 
-	 [8] = { "random", 0666, &random_fops, FMODE_NOWAIT },
-	 [9] = { "urandom", 0666, &urandom_fops, FMODE_NOWAIT },
+	#ifdef CONFIG_SRANDOM
+	 [8] = { "random", 0666, &sfops, 0 },
+	 [9] = { "urandom", 0666, &sfops, 0 },
+	#else
+	 [8] = { "random", 0666, &random_fops, 0 },
+	 [9] = { "urandom", 0666, &urandom_fops, 0 },
+	#endif
 
 #ifdef CONFIG_PRINTK
 	[11] = { "kmsg", 0644, &kmsg_fops, 0 },
