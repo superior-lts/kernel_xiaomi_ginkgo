39c5c97b7b6ee4f1fc9c0e9ae3780686a98fcfeb
AL <168145100+k4ngcaribug@users.noreply.github.com>
2025-02-07 09:08:45 +0700

    fixUp "ARM64/dts: msm: Update GPU clock plan for trinket"
    Typo:)

--- drivers/clk/qcom/gpucc-trinket.c
+++ drivers/clk/qcom/gpucc-trinket.c
@@ -190,7 +190,7 @@ static struct clk_rcg2 gpu_cc_gmu_clk_src = {
 static const struct freq_tbl ftbl_gpu_cc_gx_gfx3d_clk_src[] = {
 	F(320000000, P_GPU_CC_PLL1_OUT_AUX2, 2, 0, 0),
 	F(375000000, P_GPU_CC_PLL1_OUT_AUX2, 2, 0, 0),
-	F(456000000, P_GPU_CC_PLL0_OUT_AUX2, 2, 0, 0),
+	F(465000000, P_GPU_CC_PLL0_OUT_AUX2, 2, 0, 0),
 	F(560000000, P_GPU_CC_PLL0_OUT_AUX2, 2, 0, 0),
 	F(650000000, P_GPU_CC_PLL0_OUT_AUX2, 2, 0, 0),
 	F(745000000, P_GPU_CC_PLL0_OUT_AUX2, 2, 0, 0),
