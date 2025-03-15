26337ca25b1989ef6c5fa8d503ba4e05d39be64a
k4ngcaribug <neophyteplayground@gmail.com>
2025-03-15 20:20:49 +0700

    'techpack/data/' merge tag 'LA.UM.9.1.r1-12900-SMxxx0.QSSI13.0'

--- techpack/data/drivers/rmnet/shs/rmnet_shs_wq.c
+++ techpack/data/drivers/rmnet/shs/rmnet_shs_wq.c
@@ -546,11 +546,11 @@ void rmnet_shs_wq_update_hstat_rps_msk(struct rmnet_shs_wq_hstat_s *hstat_p)
 
 			/* Update ep tput stats while we're here */
 			if (hstat_p->skb_tport_proto == IPPROTO_TCP) {
-				rm_err("SHS_UDP: adding TCP bps %llu to ep_total %llu ep name %s",
+				rm_err("SHS_UDP: adding TCP bps %lu to ep_total %lu ep name %s",
 				       hstat_p->rx_bps, ep->tcp_rx_bps, node_p->dev->name);
 				ep->tcp_rx_bps += hstat_p->rx_bps;
 			} else if (hstat_p->skb_tport_proto == IPPROTO_UDP) {
-				rm_err("SHS_UDP: adding UDP rx_bps %llu to ep_total %llu ep name %s",
+				rm_err("SHS_UDP: adding UDP rx_bps %lu to ep_total %lu ep name %s",
 				       hstat_p->rx_bps, ep->udp_rx_bps, node_p->dev->name);
 				ep->udp_rx_bps += hstat_p->rx_bps;
 			}
@@ -2237,7 +2237,7 @@ int rmnet_shs_wq_get_max_flows_per_cluster(u16 cpu)
 		end_core = MAX_CPUS;
 	}
 
-	for (start_core = 0; start_core < end_core; start_core++) {
+	for (start_core; start_core < end_core; start_core++) {
 		cpu_flows = rmnet_shs_wq_get_num_cpu_flows(start_core);
 		if (cpu_flows > max_flows)
 			max_flows = cpu_flows;
