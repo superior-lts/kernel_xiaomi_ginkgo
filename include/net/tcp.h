9dc6d94f835840a1e7742a9ee55b10a394d86c88
AnymoreProject <anymore.dev13@gmail.com>
2026-04-25 10:22:15 +0700

    BACKPORT: net: tcp: added  bbrplus support ref: https://github.com/UJX6N/bbrplus-5.4
    Signed-off-by: kylieeXD <khayloaftck@gmail.com>
    Signed-off-by: Anymore Project <anymore.dev13@gmail.com>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -1056,6 +1056,7 @@ struct tcp_congestion_ops {
 	void (*pkts_acked)(struct sock *sk, const struct ack_sample *sample);
 	/* pick target number of segments per TSO/GSO skb (optional): */
 	u32 (*tso_segs)(struct sock *sk, unsigned int mss_now);
+	u32 (*tso_segs_goal)(struct sock *sk);
 	/* returns the multiplier used in tcp_sndbuf_expand (optional) */
 	u32 (*sndbuf_expand)(struct sock *sk);
 	/* react to a specific lost skb (optional) */
